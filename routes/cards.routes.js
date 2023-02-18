e024b558ad8d68574617b73937bf2ed402267abd
devmegahand <dev@mhand.ru>
2023-02-18 17:23:10 +0400

    Delete Oleg

--- routes/cards.routes.js
+++ routes/cards.routes.js
@@ -4,9 +4,7 @@ const cardsController = require('../controller/cards.controller')
 
 router.get('/cards', cardsController.getCardsAndNumbers)
 
-router.get('/oleg', function(req,res){
-	res.send('Oleg s DR zaebal 4ort blyat')
-})
+
 router.get('/test', function (req,res){
 	let test = {id:process.env.S3_ID, key:process.env.S3_ACC_KEY, bucket:process.env.S3_BUCKET}
 	res.send(test)
